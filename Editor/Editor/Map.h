1d62ebb0b769ccb34516af6ae9acce5a166d91d0
yuma <shibaharayuma@yahoo.co.jp>
2019-03-25 00:57:06 +0900

    Editor-v0.0.1
    Editorフォルダ・プロジェクトファイルを作成した
    また、Map.hを作成した

---
+++ Editor/Editor/Map.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <vector>
+class Event;
+
+class Map
+{
+public:
+	Map();
+	~Map();
+
+	int& getSizex() {
+		return mSizex;
+	}
+
+	int& getSizey() {
+		return mSizey;
+	}
+
+	int**& getChip() {
+		return mChip;
+	}
+
+	int*& getAlphaChip() {
+		return mAlphaChip;
+	}
+
+private:
+	int mSizex, mSizey;
+	int **mChip;
+	int *mAlphaChip;
+
+	std::vector<Event> ev;
+};
+
